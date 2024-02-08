c3ae62201236276cc85c67f7021215aef2f01f45
Shinjan Bandyopadhyay <bandy.shinjan@gmail.com>
2024-02-07 20:24:25 -0500

    something

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -146,7 +146,7 @@ public class SwerveSubsystem implements Subsystem {
         consumer_chasis, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
             new PIDConstants(3.1, 0.0, 0.02), // Translation PID constants
-            new PIDConstants(2, 0.0, 0.0), // Rotation PID constants
+            new PIDConstants(1.3, 0.0, 0.02), // Rotation PID constants
             4.5, // Max module speed, in m/s
             0.70832, // Drive base radius in meters. Distance from robot center to furthest module.
             new ReplanningConfig() // Default path replanning config. See the API for the options here
